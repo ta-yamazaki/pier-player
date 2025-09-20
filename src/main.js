3e0469eea1773872e481cc7ce7eea3e6007eac44
takashi.yamazaki <takashi.yamazaki@aideal.world>
2025-09-20 10:59:30 +0900

    自動アップデート

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import {app, BrowserWindow} from 'electron';
+import {autoUpdater} from 'electron-updater'
 import {registerIpcHandlers} from './ipc/handlers.js';
 import {createWindows, initMainWindow} from "./windows/windows.js";
 
 app.whenReady().then(() => {
     createWindows();
     registerIpcHandlers();
+
+    app.on('ready', () => {
+        autoUpdater.checkForUpdatesAndNotify()
+    })
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0)
