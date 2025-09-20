ce3efe48334788ae8449f537b975b34dd3dd2443
takashi.yamazaki <takashi.yamazaki@aideal.world>
2025-09-20 23:15:59 +0900

    check update

--- src/main.js
+++ src/main.js
@@ -6,11 +6,54 @@ import {createWindows, initMainWindow} from "./windows/windows.js";
 const {autoUpdater} = electronUpdater;
 
 app.whenReady().then(() => {
-    autoUpdater.checkForUpdatesAndNotify()
 
     createWindows();
     registerIpcHandlers();
+
+    checkForUpdatesDialog();
 });
+
+
+function checkForUpdatesDialog() {
+    autoUpdater.checkForUpdates();
+
+    // 更新がある場合
+    autoUpdater.on('update-available', (info) => {
+        dialog.showMessageBox({
+            type: 'info',
+            title: 'アップデートがあります',
+            message: `新しいバージョン ${info.version} が利用可能です。ダウンロードしますか？`,
+            buttons: ['はい', 'いいえ']
+        }).then(result => {
+            if (result.response === 0) {
+                autoUpdater.downloadUpdate();
+            }
+        });
+    });
+
+    // 更新がない場合
+    autoUpdater.on('update-not-available', () => {
+        console.log('最新バージョンです');
+    });
+
+    // ダウンロード完了時
+    autoUpdater.on('update-downloaded', (info) => {
+        dialog.showMessageBox({
+            type: 'info',
+            title: 'アップデート準備完了',
+            message: 'アプリを再起動すると最新になります',
+            buttons: ['今すぐ再起動', '後で']
+        }).then(result => {
+            if (result.response === 0) {
+                autoUpdater.quitAndInstall();
+            }
+        });
+    });
+
+    autoUpdater.on('error', (err) => {
+        console.error('アップデートエラー:', err);
+    });
+}
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0)
