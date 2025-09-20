8a0b0dd7b83a88c665d7dfcbe28c5eae095643e7
takashi.yamazaki <takashi.yamazaki@aideal.world>
2025-09-20 17:36:41 +0900

    autoUpdater修正

--- release-force.js
+++ release-force.js
@@ -4,5 +4,6 @@ import pkg from "./package.json" with { type: "json" };
 const version = pkg.version;
 
 execSync(`git tag -d v${version}`, { stdio: "inherit" });
+execSync(`git push origin --delete v${version}`, { stdio: "inherit" });
 execSync(`git tag v${version}`, { stdio: "inherit" });
 execSync(`git push origin v${version}`, { stdio: "inherit" });
