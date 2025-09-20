0669eee10ccdff183e1fe060dc9e8cc4c3d7bdfb
takashi.yamazaki <takashi.yamazaki@aideal.world>
2025-09-20 22:57:18 +0900

    fix

--- src/ipc/commonHandlers.js
+++ src/ipc/commonHandlers.js
@@ -1,10 +1,20 @@
-import { ipcMain, app } from 'electron';
+import {app, ipcMain} from 'electron';
 import pkg from 'electron-updater';
 const {autoUpdater} = pkg;
 
 export const registerCommonHandlers = () => {
     ipcMain.handle('getVersion', () => app.getVersion());
-    ipcMain.handle('checkUpdate', () => {
-       return autoUpdater.checkForUpdates()
+    ipcMain.handle('checkUpdate', async () => {
+        const result = await autoUpdater.checkForUpdates();
+        if (!result || !result.updateInfo) {
+            return {updateAvailable: false};
+        }
+        // プレーンオブジェクトにして返す
+        return {
+            updateAvailable: true,
+            version: result.updateInfo.version,
+            releaseNotes: result.updateInfo.releaseNotes,
+            releaseName: result.updateInfo.releaseName,
+        };
     });
 };
