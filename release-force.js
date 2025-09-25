7025d06b028ef2b31340f7c13d1edcc31a0123a0
takashi.yamazaki <takashi.yamazaki@aideal.world>
2025-09-25 22:01:58 +0900

    リリースコマンド

---
+++ release-force.js
@@ -0,0 +1,9 @@
+import { execSync } from "child_process";
+import pkg from "./package.json" with { type: "json" };
+
+const version = pkg.version;
+
+execSync(`git tag -d v${version}`, { stdio: "inherit" });
+execSync(`git push origin --delete v${version}`, { stdio: "inherit" });
+execSync(`git tag v${version}`, { stdio: "inherit" });
+execSync(`git push origin v${version}`, { stdio: "inherit" });
